08a75afa019b51695c15b2a6f405aefd762dec0b
DANIEL DA SILVA CHISTOVAO <T2501DNL@prestadorcbmp.com.br>
2019-05-07 08:01:34 -0300

    dependencias

---
+++ tarefasControle/src/main/java/com/example/tarefasControle/TarefasControleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarefasControle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefasControleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefasControleApplication.class, args);
+	}
+
+}
